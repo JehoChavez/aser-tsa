80652bfbc4df94f0586a67a14924fe242dcbd82d
Jeho Chavez <chavezjehonatan@gmail.com>
2024-03-18 14:38:53 -0600

    Seed the estados and municipios tables

---
+++ server/seeders/20240318202831-municipios.js
@@ -0,0 +1,28 @@
+"use strict";
+const municipios = require("./municipios");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Municipios", municipios, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Municipios", null, {});
+  },
+};
