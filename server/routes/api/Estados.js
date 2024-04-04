d8dbffb5ae33ea28c7eacb445d5854153f27711c
Jeho Chavez <chavezjehonatan@gmail.com>
2024-04-04 17:24:18 -0600

    Remove unnecessary console.log

--- server/routes/api/Estados.js
+++ server/routes/api/Estados.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { Estado } = require("../../models");
 
 router.get("/", async (req, res) => {
-  console.log(Estado);
   const listOfEstados = await Estado.findAll();
 
   res.json(listOfEstados);
