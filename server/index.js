8f30acdb3ca9cd03598775e9c27f00eca5cedce3
Jeho Chavez <chavezjehonatan@gmail.com>
2024-04-09 17:10:32 -0600

    Add basic error handler

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ app.use("/api/estados", estadosRouter);
 app.use("/api/municipios", municipiosRouter);
 app.use("/api/productos", productosRouter);
 
+// Error handler
+app.use((err, req, res, next) => {
+  res.send("Oh boy, something went wrong!");
+});
+
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("Server running on port 3001");
