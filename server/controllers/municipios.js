c6c9838079369e1e11bb32364432e90c474b8bf7
Jeho Chavez <chavezjehonatan@gmail.com>
2024-04-10 19:25:54 -0600

    Receive estado ID in body instead of params.

--- server/controllers/municipios.js
+++ server/controllers/municipios.js
@@ -2,25 +2,25 @@ const { Municipio } = require("../models");
 const ExpressError = require("../utils/ExpressError");
 const CustomResponse = require("../utils/CustomResponse");
 
-module.exports.noId = (req, res) => {
-  throw new ExpressError("ID de estado es necesario", 400);
-};
-
 module.exports.getMunicipio = async (req, res) => {
-  const listOfMunicipios = await Municipio.findAll({
-    where: {
-      estadoId: req.params.estado,
-    },
-    attributes: {
-      exclude: ["createdAt", "updatedAt"],
-    },
-  });
-
-  if (!listOfMunicipios[0]) {
-    throw new ExpressError("ID de estado no es válido (1-32)", 400);
+  if (!req.body.estado) {
+    throw new ExpressError("ID de estado es necesario", 400);
   } else {
-    const response = new CustomResponse(listOfMunicipios);
+    const listOfMunicipios = await Municipio.findAll({
+      where: {
+        estadoId: req.body.estado,
+      },
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
 
-    res.json(response);
+    if (!listOfMunicipios[0]) {
+      throw new ExpressError("ID de estado no es válido (1-32)", 400);
+    } else {
+      const response = new CustomResponse(listOfMunicipios);
+
+      res.json(response);
+    }
   }
 };
