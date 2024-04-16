2b58ce8eb13c14f72e92bdeaa192d0b2aa18a37c
Jeho Chavez <chavezjehonatan@gmail.com>
2024-04-16 17:08:53 -0600

    Add indexes for more efficient querying

---
+++ server/migrations/20240416222823-add-indexes.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addIndex("agentes", ["clave"], {
+      indexName: "claveIndex",
+    });
+
+    await queryInterface.addIndex("polizas", ["noPoliza"], {
+      indexName: "polizaIndex",
+    });
+
+    await queryInterface.addIndex("endosos", ["polizaId"], {
+      indexName: "endosoPolizaIdIndex",
+    });
+
+    await queryInterface.addIndex("recibos", ["polizaId"], {
+      indexName: "reciboPolizaIdIndex",
+    });
+
+    await queryInterface.addIndex("recibos", [
+      "fechaInicio",
+      {
+        indexName: "reciboInicioIndex",
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeIndex("agentes", "claveIndex");
+
+    await queryInterface.removeIndex("polizas", "polizaIndex");
+
+    await queryInterface.removeIndex("endosos", "endosoPolizaIdIndex");
+
+    await queryInterface.removeIndex("recibos", "reciboPolizaIdIndex");
+
+    await queryInterface.removeIndex("recibos", "reciboInicioIndex");
+  },
+};
