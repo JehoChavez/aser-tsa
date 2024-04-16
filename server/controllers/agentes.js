ffd72a0983fc130e874adb6abc1315b45136e05b
Jeho Chavez <chavezjehonatan@gmail.com>
2024-04-15 18:49:27 -0600

    Filter agentes by aseguradora

--- server/controllers/agentes.js
+++ server/controllers/agentes.js
@@ -1,21 +1,27 @@
+const { Op } = require("sequelize");
 const { Agente } = require("../models");
 const CustomResponse = require("../utils/CustomResponse");
 const ExpressError = require("../utils/ExpressError");
 
 module.exports.getAgentes = async (req, res) => {
-  const { aseguradoraId } = req.body;
+  const { aseguradoras } = req.query;
 
   let listOfAgentes = [];
 
-  if (aseguradoraId) {
+  if (aseguradoras) {
+    const aseguradoraIds = aseguradoras.split(",");
     listOfAgentes = await Agente.findAll({
       where: {
-        aseguradoraId: aseguradoraId,
+        aseguradoraId: {
+          [Op.in]: aseguradoraIds,
+        },
       },
     });
   } else {
     listOfAgentes = await Agente.findAll();
   }
+
+  if (!listOfAgentes[0]) throw new ExpressError("Agentes no encontrados", 404);
 
   const response = new CustomResponse(listOfAgentes);
 
