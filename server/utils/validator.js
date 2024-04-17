37916efb1451cf180a866399e567ace588538cfd
Jeho Chavez <chavezjehonatan@gmail.com>
2024-04-16 18:29:32 -0600

    Estado is required

--- server/utils/validator.js
+++ server/utils/validator.js
@@ -3,7 +3,7 @@ const joi = require("joi");
 const validator = (schema) => (payload) => schema.validate(payload);
 
 const estadoIdSchema = joi.object({
-  estado: joi.number().min(1).max(32),
+  estado: joi.number().min(1).max(32).required(),
 });
 
 module.exports.validateEstadoId = validator(estadoIdSchema);
