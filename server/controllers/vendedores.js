a887f5a3212071fb45dd160ee7f3ffbe2dcc82a8
Jeho Chavez <chavezjehonatan@gmail.com>
2024-04-22 17:21:38 -0600

    Post vendedor route

--- server/controllers/vendedores.js
+++ server/controllers/vendedores.js
@@ -1,6 +1,7 @@
 const { Vendedor } = require("../models");
 const ExpressError = require("../utils/ExpressError");
 const CustomResponse = require("../utils/CustomResponse");
+const { validateVendedor } = require("../utils/validator");
 
 module.exports.getVendedores = async (req, res) => {
   const listOfVendedores = await Vendedor.findAll();
@@ -9,6 +10,18 @@ module.exports.getVendedores = async (req, res) => {
     throw new ExpressError("Vendedores no encontrados", 404);
 
   const response = new CustomResponse(listOfVendedores);
+
+  res.json(response);
+};
+
+module.exports.postVendedor = async (req, res) => {
+  const { error, value } = validateVendedor(req.body);
+
+  if (error) throw new ExpressError(error.details[0].message, 400);
+
+  const nuevoVendedor = await Vendedor.create(value);
+
+  const response = new CustomResponse(nuevoVendedor);
 
   res.json(response);
 };
--- server/routes/api/vendedores.js
+++ server/routes/api/vendedores.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../../utils/catchAsync");
-const { getVendedores } = require("../../controllers/vendedores");
+const { getVendedores, postVendedor } = require("../../controllers/vendedores");
 
-router.route("/").get(catchAsync(getVendedores));
+router.route("/").get(catchAsync(getVendedores)).post(catchAsync(postVendedor));
 
 module.exports = router;
