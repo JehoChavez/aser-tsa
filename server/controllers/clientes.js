8df69318dd6c6e15acfa75e1469fbb1be3f2a038
Jeho Chavez <chavezjehonatan@gmail.com>
2024-04-23 17:28:49 -0600

    Post cliente route

--- server/controllers/clientes.js
+++ server/controllers/clientes.js
@@ -1,6 +1,7 @@
 const { Cliente } = require("../models");
 const CustomResponse = require("../utils/CustomResponse");
 const ExpressError = require("../utils/ExpressError");
+const { validateCliente } = require("../utils/validator");
 
 module.exports.getClientes = async (req, res) => {
   const listOfClientes = await Cliente.findAll();
@@ -13,4 +14,16 @@ module.exports.getClientes = async (req, res) => {
 module.exports.getCliente = async (req, res) => {
   const id = req.params.id;
   res.send(`Cliente ${id}`);
+};
+
+module.exports.postCliente = async (req, res) => {
+  const { error, value } = validateCliente(req.body);
+
+  if (error) throw new ExpressError(error.details[0].message, 400);
+
+  const cliente = await Cliente.create(value);
+
+  const response = new CustomResponse(cliente);
+
+  res.json(response);
 };
--- server/routes/api/clientes.js
+++ server/routes/api/clientes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../../utils/catchAsync");
-const { getClientes, getCliente } = require("../../controllers/clientes");
+const {
+  getClientes,
+  getCliente,
+  postCliente,
+} = require("../../controllers/clientes");
 
-router.route("/").get(catchAsync(getClientes));
+router.route("/").get(catchAsync(getClientes)).post(catchAsync(postCliente));
 
-router.route("/:id").get(catchAsync(getCliente))
+router.route("/:id").get(catchAsync(getCliente));
 
 module.exports = router;
