6707063ad7adeda2cc97d3286b8f85236e3da673
Jeho Chavez <chavezjehonatan@gmail.com>
2024-05-06 17:09:09 -0600

    Move ramos validation logic to middleware

--- server/controllers/ramos.js
+++ server/controllers/ramos.js
@@ -1,7 +1,6 @@
 const { Ramo } = require("../models");
 const ExpressError = require("../utils/ExpressError");
 const CustomResponse = require("../utils/CustomResponse");
-const { validateRamo, validateGenericId } = require("../utils/validator");
 
 module.exports.getRamos = async (req, res) => {
   const listOfRamos = await Ramo.findAll();
@@ -12,11 +11,7 @@ module.exports.getRamos = async (req, res) => {
 };
 
 module.exports.postRamo = async (req, res) => {
-  const { error, value } = validateRamo(req.body);
-
-  if (error) throw new ExpressError(error.details[0].message, 400);
-
-  const nuevoRamo = await Ramo.create(value);
+  const nuevoRamo = await Ramo.create(req.body);
 
   const response = new CustomResponse(nuevoRamo);
 
@@ -24,11 +19,7 @@ module.exports.postRamo = async (req, res) => {
 };
 
 module.exports.deleteRamo = async (req, res) => {
-  const { error, value } = validateGenericId(req.params);
-
-  if (error) throw new ExpressError(error.details[0].message, 400);
-
-  const ramo = await Ramo.findByPk(value.id);
+  const ramo = await Ramo.findByPk(req.params.id);
 
   if (!ramo) throw new ExpressError("ramo No Encontrado", 404);
 
@@ -40,23 +31,11 @@ module.exports.deleteRamo = async (req, res) => {
 };
 
 module.exports.updateRamo = async (req, res) => {
-  const { error: ramoUpdateIdError, value: ramoUpdateId } = validateGenericId(
-    req.params
-  );
-
-  if (ramoUpdateIdError)
-    throw new ExpressError(ramoUpdateIdError.details[0].message, 400);
-
-  const ramo = await Ramo.findByPk(ramoUpdateId.id);
+  const ramo = await Ramo.findByPk(req.params.id);
 
   if (!ramo) throw new ExpressError("ramo No Encontrado", 404);
 
-  const { error: ramoDataError, value: ramoData } = validateRamo(req.body);
-
-  if (ramoDataError)
-    throw new ExpressError(ramoDataError.details[0].message, 400);
-
-  ramo.ramo = ramoData.ramo;
+  ramo.set(req.body);
 
   const updated = await ramo.save();
 
--- server/routes/api/ramos.js
+++ server/routes/api/ramos.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../../utils/catchAsync");
-const ramos = require("../../controllers/ramos");
+const {
+  getRamos,
+  postRamo,
+  deleteRamo,
+  updateRamo,
+} = require("../../controllers/ramos");
+const { validateRamo, validateGenericId } = require("../../utils/validator");
 
 router
   .route("/")
-  .get(catchAsync(ramos.getRamos))
-  .post(catchAsync(ramos.postRamo));
+  .get(catchAsync(getRamos))
+  .post(validateRamo, catchAsync(postRamo));
 
 router
   .route("/:id")
-  .delete(catchAsync(ramos.deleteRamo))
-  .put(catchAsync(ramos.updateRamo));
+  .delete(validateGenericId, catchAsync(deleteRamo))
+  .put(validateGenericId, validateRamo, catchAsync(updateRamo));
 
 module.exports = router;
