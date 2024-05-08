a8db5c37823eb05bb7b516337e4a273cd7bed75f
Jeho Chavez <chavezjehonatan@gmail.com>
2024-05-07 18:06:40 -0600

    postEndoso route

--- server/controllers/endosos.js
+++ server/controllers/endosos.js
@@ -1,6 +1,6 @@
 // Some functions are imported in polizas router
 // Polizas controller already has a lot of code
-const { Endoso } = require("../models");
+const { Endoso, Poliza, Recibo, sequelize } = require("../models");
 const CustomResponse = require("../utils/CustomResponse");
 const ExpressError = require("../utils/ExpressError");
 
@@ -24,4 +24,53 @@ module.exports.getEndoso = async (req, res) => {
   const response = new CustomResponse(endoso);
 
   res.json(response);
+};
+
+module.exports.postEndoso = async (req, res) => {
+  const endosoData = req.body.endoso;
+  const recibosData = req.body.recibos;
+
+  const poliza = await Poliza.findByPk(endosoData.polizaId);
+
+  if (!poliza) throw new ExpressError("poliza no encontrada", 404);
+
+  const existingEndoso = await Endoso.findOne({
+    where: {
+      endoso: endosoData.endoso,
+      polizaId: endosoData.polizaId,
+    },
+  });
+
+  if (existingEndoso) throw new ExpressError("endoso ya existe", 400);
+
+  const t = await sequelize.transaction();
+
+  try {
+    const endoso = await Endoso.create(endosoData, { transaction: t });
+
+    const responseBody = { endoso };
+
+    if (recibosData) {
+      const recibos = await Promise.all(
+        recibosData.map(async (reciboData) => {
+          const recibo = await Recibo.create(
+            { ...reciboData, polizaId: poliza.id, endosoId: endoso.id },
+            { transaction: t }
+          );
+          return recibo;
+        })
+      );
+      responseBody.recibos = recibos;
+    }
+
+    await t.commit();
+
+    const response = new CustomResponse(responseBody, 200);
+
+    res.json(response);
+  } catch (error) {
+    await t.rollback();
+
+    throw new ExpressError(error);
+  }
 };
--- server/routes/api/endosos.js
+++ server/routes/api/endosos.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../../utils/catchAsync");
-const { getEndoso } = require("../../controllers/endosos");
-const { validateGenericId } = require("../../utils/validator");
+const { getEndoso, postEndoso } = require("../../controllers/endosos");
+const { validateGenericId, validateEndoso } = require("../../utils/validator");
+
+router.route("/").post(validateEndoso, catchAsync(postEndoso));
 
 router.route("/:id").get(validateGenericId, catchAsync(getEndoso));
 
