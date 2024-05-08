b5bb6907036bfb8cce4103b1530208aa33ec3967
Jeho Chavez <chavezjehonatan@gmail.com>
2024-05-08 17:09:10 -0600

    Validate recibo query

--- server/routes/api/recibos.js
+++ server/routes/api/recibos.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { getRecibos } = require("../../controllers/recibos");
 const catchAsync = require("../../utils/catchAsync");
+const { validateReciboQuery } = require("../../utils/validator");
 
-router.route("/").get(catchAsync(getRecibos));
+router.route("/").get(validateReciboQuery, catchAsync(getRecibos));
 
 module.exports = router;
