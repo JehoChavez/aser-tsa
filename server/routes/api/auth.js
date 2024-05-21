5ba3b3b00b401d73d619846830e3377460127647
Jeho Chavez <chavezjehonatan@gmail.com>
2024-05-21 16:31:21 -0600

    Move auth router outside api folder

--- server/routes/api/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login } = require("../../controllers/auth");
-const { validateLogin } = require("../../utils/validator");
-
-router.route("/login").post(validateLogin, login);
-
-module.exports = router;
