9cc8c1c3da7a8b26b18a784e1d536bbfc396c4bd
Jeho Chavez <chavezjehonatan@gmail.com>
2024-05-21 16:18:18 -0600

    Use session secrent env variable

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: process.env.AUTH_PASSWORD,
+    secret: process.env.SESSION_SECRET,
     store: sessionStore,
     resave: false,
     saveUninitialized: false,
