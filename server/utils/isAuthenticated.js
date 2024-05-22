ed36e6300e00f750cc6ad2d090c229bb1c6e5851
Jeho Chavez <chavezjehonatan@gmail.com>
2024-05-22 17:54:23 -0600

    Send correct response when user is not authenticated

--- server/utils/isAuthenticated.js
+++ server/utils/isAuthenticated.js
@@ -1,7 +1,10 @@
+const CustomResponse = require("./CustomResponse");
+
 module.exports = (req, res, next) => {
   if (req.session && req.session.isAuthenticated) {
     return next();
   }
   // TODO: redirect to login view
-  res.json("User is not authenticated");
+  const response = new CustomResponse(null, 401, "unathorized");
+  res.status(response.status).json(response);
 };
