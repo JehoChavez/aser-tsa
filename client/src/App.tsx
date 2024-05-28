02aae733bfe9f5137a69cde30bfff98039d3d2d6
Jeho Chavez <chavezjehonatan@gmail.com>
2024-05-28 17:47:01 -0600

    Basic NotFound page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 
-const router = createBrowserRouter([{ path: "/login", element: <p>Login</p> }]);
+const router = createBrowserRouter([
+  { path: "/login", element: <p>Login</p> },
+  { path: "*", element: <NotFound /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>La página a la que intentas acceder no existe</p>
+    </div>
+  );
+};
+
+export default NotFound;
