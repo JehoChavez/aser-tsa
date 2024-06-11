54987b86fd35a01fcb1fce3eb850e5129eda6d00
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-10 18:07:58 -0600

    Remove opacity

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { GridLoader } from "react-spinners";
 
 const Loading = () => {
   return (
-    <div className="h-full flex flex-col items-center justify-center opacity-50">
+    <div className="h-full flex flex-col items-center justify-center">
       <GridLoader color="navy" />
     </div>
   );
