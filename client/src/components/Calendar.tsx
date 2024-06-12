d3c5bc50bdf387ef6b794c7279155e71608cdd09
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-11 18:43:17 -0600

    Move all calendar functionality to context

--- client/src/components/Calendar.tsx
+++ client/src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useContext } from "react";
 import {
   Calendar,
   Event,
@@ -8,147 +8,19 @@ import {
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
 import "moment/locale/es";
-import axios from "axios";
-import {
-  CustomEvent,
-  DateRange,
-  Recibo,
-  Renovacion,
-} from "../types/interfaces";
+import { CustomEvent, DateRange } from "../types/interfaces";
 import Modal from "./Modal";
 import Loading from "./Loading";
 import CalendarEvent from "./CalendarEvent";
 import Pendientes from "./Pendientes";
-import { PendientesInterface } from "../types/interfaces";
+import { CalendarContext } from "../store/calendar-context";
 
 moment.locale("es");
 
 const localizer = momentLocalizer(moment);
 
 const CalendarComponent = () => {
-  // Set initial range to month start and end
-  const [dateRange, setDateRange] = useState<DateRange>({
-    start: moment().startOf("month").toDate(),
-    end: moment().endOf("month").toDate(),
-  });
-  const [recibos, setRecibos] = useState<Recibo[]>([]);
-  const [renovaciones, setRenovaciones] = useState<Renovacion[]>([]);
-  const [events, setEvents] = useState<Event[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [eventPendientes, setEventPendientes] =
-    useState<PendientesInterface | null>(null);
-
-  const rangeChangeHandler = (range: DateRange) => {
-    setDateRange(range);
-    setEventPendientes(null);
-  };
-
-  // Fetch pendientes each time dateRange changes
-  const getEvents = useCallback(async () => {
-    // Convert dates to iso date strings (api expects string)
-    const startDate = dateRange.start.toISOString().split("T")[0];
-    const endDate = dateRange.end.toISOString().split("T")[0];
-    try {
-      setIsLoading(true);
-      const response = await axios.get("http://localhost:3000/api/pendientes", {
-        params: { desde: startDate, hasta: endDate },
-        withCredentials: true,
-      });
-      const fetchedRecibos: Recibo[] = response.data.content.cobranza;
-      setRecibos(fetchedRecibos);
-      const fetchedRenovaciones: Renovacion[] =
-        response.data.content.renovaciones;
-      setRenovaciones(fetchedRenovaciones);
-      setIsLoading(false);
-    } catch (error) {
-      setIsLoading(false);
-      console.log(error);
-    }
-  }, [dateRange]);
-
-  useEffect(() => {
-    getEvents();
-  }, [dateRange]);
-
-  useEffect(() => {
-    // Convert recibos and renovaciones to events wuth count
-    const rangeEvents: CustomEvent[] = [];
-    const countedRecibos: CustomEvent[] = recibos.reduce(
-      (acc: CustomEvent[], recibo) => {
-        const reciboDate = new Date(recibo.fechaInicio);
-
-        const existingEntry: CustomEvent | undefined = acc.find((entry) => {
-          return entry.start?.getTime() === reciboDate.getTime();
-        });
-
-        if (existingEntry) {
-          existingEntry.count++;
-        } else {
-          acc.push({
-            start: reciboDate,
-            end: reciboDate,
-            type: "cobranza",
-            count: 1,
-          });
-        }
-
-        return acc;
-      },
-      []
-    );
-
-    const countedRenovaciones: CustomEvent[] = renovaciones.reduce(
-      (acc: CustomEvent[], renovacion) => {
-        const renovacionDate = new Date(renovacion.finVigencia);
-
-        const existingEntry: CustomEvent | undefined = acc.find(
-          (entry) => entry.start?.getTime() === renovacionDate.getTime()
-        );
-
-        if (existingEntry) {
-          existingEntry.count++;
-        } else {
-          acc.push({
-            start: renovacionDate,
-            end: renovacionDate,
-            type: "renovacion",
-            count: 1,
-          });
-        }
-
-        return acc;
-      },
-      []
-    );
-
-    rangeEvents.push(...countedRecibos);
-    rangeEvents.push(...countedRenovaciones);
-
-    // Set events
-    setEvents(rangeEvents);
-  }, [recibos, renovaciones]);
-
-  const selectEventHandler = (event: CustomEvent) => {
-    if (event.type === "cobranza") {
-      const eventRecibos = recibos.filter(
-        (recibo) =>
-          new Date(recibo.fechaInicio).getTime() === event.start?.getTime()
-      );
-      setEventPendientes({
-        type: "cobranza",
-        pendientes: eventRecibos,
-      });
-    } else if (event.type === "renovacion") {
-      const eventRenovaciones = renovaciones.filter(
-        (renovacion) =>
-          new Date(renovacion.finVigencia).getTime() === event.start?.getTime()
-      );
-      setEventPendientes({
-        type: "renovaciones",
-        pendientes: eventRenovaciones,
-      });
-    }
-  };
+  const calendarContext = useContext(CalendarContext);
 
   return (
     <>
@@ -159,26 +31,30 @@ const CalendarComponent = () => {
           endAccessor="end"
           culture="es"
           views={["month"]}
-          events={events}
+          events={calendarContext.events}
           components={{
             event: CalendarEvent as React.ComponentType<EventProps<Event>>,
           }}
-          onRangeChange={(range) => rangeChangeHandler(range as DateRange)}
-          onSelectEvent={(event) => selectEventHandler(event as CustomEvent)}
+          onRangeChange={(range) =>
+            calendarContext.changeRange(range as DateRange)
+          }
+          onSelectEvent={(event) =>
+            calendarContext.selectDay(event as CustomEvent)
+          }
         />
       </div>
-      {isLoading && (
+      {calendarContext.isLoading && (
         <Modal size="small">
           <Loading />
         </Modal>
       )}
-      {eventPendientes && (
+      {calendarContext.dayPendientes && (
         <Modal
           size="large"
           closeBtn={true}
-          onClose={() => setEventPendientes(null)}
+          onClose={calendarContext.closePendientesModal}
         >
-          <Pendientes pendientes={eventPendientes} />
+          <Pendientes pendientes={calendarContext.dayPendientes} />
         </Modal>
       )}
     </>
--- client/src/pages/Calendario.tsx
+++ client/src/pages/Calendario.tsx
@@ -1,13 +1,179 @@
+import { useState, useEffect } from "react";
 import CalendarComponent from "../components/Calendar";
 import {
   CalendarContext,
   defaultCalendarContextValue,
 } from "../store/calendar-context";
+import axios from "axios";
+import {
+  Recibo,
+  Renovacion,
+  CustomEvent,
+  PendientesInterface,
+  DateRange,
+} from "../types/interfaces";
 
 const Calendario = () => {
+  const [isLoading, setIsLoading] = useState(
+    defaultCalendarContextValue.isLoading
+  );
+  const [range, setRange] = useState(defaultCalendarContextValue.range);
+  const [pendientes, setPendientes] = useState(
+    defaultCalendarContextValue.pendientes
+  );
+  const [events, setEvents] = useState(defaultCalendarContextValue.events);
+  const [selectedDay, setSelectedDay] = useState<Date | null>(
+    defaultCalendarContextValue.selectedDay
+  );
+  const [type, setType] = useState<"cobranza" | "renovacion" | null>(
+    defaultCalendarContextValue.type
+  );
+  const [dayPendientes, setDayPendientes] = useState(
+    defaultCalendarContextValue.dayPendientes
+  );
+
+  const fetchEvents = async () => {
+    const rangePendientes: PendientesInterface = {
+      cobranza: [],
+      renovacion: [],
+    };
+    // Convert dates to iso date strings (api expects string)
+    try {
+      setIsLoading(true);
+      const response = await axios.get("http://localhost:3000/api/pendientes", {
+        params: {
+          desde: range.start.toISOString(),
+          hasta: range.end.toISOString(),
+        },
+        withCredentials: true,
+      });
+      const fetchedRecibos: Recibo[] = response.data.content.cobranza;
+      rangePendientes.cobranza = fetchedRecibos;
+
+      const fetchedRenovaciones: Renovacion[] =
+        response.data.content.renovaciones;
+      rangePendientes.renovacion = fetchedRenovaciones;
+
+      setPendientes(rangePendientes);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  const changeRange = (range: DateRange) => {
+    setRange(range);
+    setSelectedDay(null);
+    setDayPendientes(null);
+    setType(null);
+  };
+
+  const selectDay = (event: CustomEvent) => {
+    setType(event.type);
+    setSelectedDay(event.start as Date);
+
+    let eventPendientes: Renovacion[] | Recibo[] = [];
+    if (event.type === "cobranza") {
+      eventPendientes = pendientes.cobranza.filter(
+        (recibo) =>
+          new Date(recibo.fechaInicio).getTime() === event.start?.getTime()
+      );
+    } else if (event.type === "renovacion") {
+      eventPendientes = pendientes.renovacion.filter(
+        (renovacion) =>
+          new Date(renovacion.finVigencia).getTime() === event.start?.getTime()
+      );
+    }
+    setDayPendientes(eventPendientes);
+  };
+
+  const closePendientesModal = () => {
+    setSelectedDay(null);
+    setType(null);
+    setDayPendientes(null);
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, [range]);
+
+  useEffect(() => {
+    // Convert recibos and renovaciones to events wuth count
+    const rangeEvents: CustomEvent[] = [];
+    const countedRecibos: CustomEvent[] = pendientes.cobranza.reduce(
+      (acc: CustomEvent[], recibo) => {
+        const reciboDate = new Date(recibo.fechaInicio);
+
+        const existingEntry: CustomEvent | undefined = acc.find((entry) => {
+          return entry.start?.getTime() === reciboDate.getTime();
+        });
+
+        if (existingEntry) {
+          existingEntry.count++;
+        } else {
+          acc.push({
+            start: reciboDate,
+            end: reciboDate,
+            type: "cobranza",
+            count: 1,
+          });
+        }
+
+        return acc;
+      },
+      []
+    );
+
+    const countedRenovaciones: CustomEvent[] = pendientes.renovacion.reduce(
+      (acc: CustomEvent[], renovacion) => {
+        const renovacionDate = new Date(renovacion.finVigencia);
+
+        const existingEntry: CustomEvent | undefined = acc.find(
+          (entry) => entry.start?.getTime() === renovacionDate.getTime()
+        );
+
+        if (existingEntry) {
+          existingEntry.count++;
+        } else {
+          acc.push({
+            start: renovacionDate,
+            end: renovacionDate,
+            type: "renovacion",
+            count: 1,
+          });
+        }
+
+        return acc;
+      },
+      []
+    );
+
+    rangeEvents.push(...countedRecibos);
+    rangeEvents.push(...countedRenovaciones);
+
+    // Set events
+    setEvents(rangeEvents);
+  }, [pendientes]);
+
   return (
     <div className="mt-16 h-full w-full">
-      <CalendarContext.Provider value={defaultCalendarContextValue}>
+      <CalendarContext.Provider
+        value={{
+          isLoading,
+          range,
+          pendientes,
+          events,
+          selectedDay,
+          type,
+          dayPendientes,
+          fetchEvents,
+          changeRange,
+          selectDay,
+          closePendientesModal,
+        }}
+      >
         <CalendarComponent />
       </CalendarContext.Provider>
     </div>
