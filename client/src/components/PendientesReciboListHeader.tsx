064421c036b45ccf4a6905cc89909566b3cf92a0
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-12 19:45:33 -0600

    Minor changes

--- client/src/components/PendientesReciboListHeader.tsx
+++ client/src/components/PendientesReciboListHeader.tsx
@@ -3,12 +3,12 @@ const PendientesReciboListHeader = () => {
     <div className="w-full p-2 bg-blue-950 text-gray-200 grid grid-cols-12 text-wrap">
       <h3 className="col-start-1 col-span-2">Cliente</h3>
       <h3 className="col-span-2">Ramo</h3>
-      <h3 className="col-span-2">Poliza</h3>
+      <h3 className="col-span-2">Póliza</h3>
       <h3>Endoso</h3>
       <h3>Serie</h3>
       <h3 className="col-span-2">Aseguradora</h3>
       <h3>Monto</h3>
-      <h3>Accion</h3>
+      <h3>Acción</h3>
     </div>
   );
 };
