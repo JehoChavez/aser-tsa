7dfb024f3eb77815158c6eb0d85f31bc61ffc987
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-18 16:50:36 -0600

    Component for recibos pendientes

--- client/src/components/Pendientes.tsx
+++ client/src/components/Pendientes.tsx
@@ -1,52 +1,12 @@
 import { useContext } from "react";
 import { CalendarContext } from "../store/calendar-context";
-import ListItem from "./ListItem";
-import ListDialog from "./LIstDialog";
-import Modal from "./Modal";
-import PendientesReciboListHeader from "./PendientesReciboListHeader";
-import PendientesReciboListItem from "./PendientesReciboListItem";
-import { Recibo, Renovacion } from "../types/interfaces";
+import RecibosPendientes from "./RecibosPendientes";
 
 const Pendientes = () => {
   const calendarContext = useContext(CalendarContext);
 
   if (calendarContext.type === "cobranza" && calendarContext.dayPendientes) {
-    return (
-      <Modal
-        size="large"
-        closeBtn={true}
-        onClose={calendarContext.closePendientesModal}
-      >
-        <ListDialog
-          title={calendarContext.type}
-          header={
-            calendarContext.type === "cobranza" ? (
-              <PendientesReciboListHeader />
-            ) : null
-          }
-        >
-          {!calendarContext.dayPendientes[0] && (
-            <h3 className="text-lg text-center">
-              No hay pendientes para esta fecha
-            </h3>
-          )}
-          {calendarContext.dayPendientes.map((pendiente) => {
-            if (calendarContext.type === "cobranza") {
-              const recibo = pendiente as Recibo;
-              return (
-                <PendientesReciboListItem recibo={recibo} key={recibo.id} />
-              );
-            } else if (calendarContext.type === "renovacion") {
-              const renovacion = pendiente as Renovacion;
-              return (
-                <ListItem key={renovacion.id}>{renovacion.noPoliza}</ListItem>
-              );
-            }
-            return null;
-          })}
-        </ListDialog>
-      </Modal>
-    );
+    return <RecibosPendientes />;
   }
 
   return <div>No pending items.</div>;
---
+++ client/src/components/RecibosPendientes.tsx
@@ -0,0 +1,37 @@
+import { useContext } from "react";
+import { CalendarContext } from "../store/calendar-context";
+import Modal from "./Modal";
+import PendientesReciboListHeader from "./PendientesReciboListHeader";
+import ListDialog from "./LIstDialog";
+import PendientesReciboListItem from "./PendientesReciboListItem";
+import { Recibo } from "../types/interfaces";
+
+const RecibosPendientes = () => {
+  const calendarContext = useContext(CalendarContext);
+  return (
+    <Modal
+      size="large"
+      closeBtn={true}
+      onClose={calendarContext.closePendientesModal}
+    >
+      <ListDialog
+        title={calendarContext.type}
+        header={<PendientesReciboListHeader />}
+      >
+        {calendarContext.dayPendientes &&
+        calendarContext.dayPendientes.length > 0 ? (
+          calendarContext.dayPendientes?.map((pendiente) => {
+            const recibo = pendiente as Recibo;
+            return <PendientesReciboListItem recibo={recibo} key={recibo.id} />;
+          })
+        ) : (
+          <h3 className="text-lg text-center">
+            No hay pendientes para esta fecha
+          </h3>
+        )}
+      </ListDialog>
+    </Modal>
+  );
+};
+
+export default RecibosPendientes;
