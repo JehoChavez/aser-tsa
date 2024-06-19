6c17232c73a47a398de89f28aa73887dd7d930d6
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-18 18:30:34 -0600

    Title and icon

--- client/src/pages/Clientes.tsx
+++ client/src/pages/Clientes.tsx
@@ -1,7 +1,19 @@
 const Clientes = () => {
   return (
-    <div className="mt-16 w-full h-full">
-      <h1>Clientes Page</h1>
+    <div className="mt-16 w-full h-full p-5 text-blue-950 text-opacity-95">
+      <span className="flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="currentColor"
+          className="bi bi-people-fill mr-2"
+          viewBox="0 0 16 16"
+        >
+          <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
+        </svg>
+        <h1 className="text-3xl">Clientes</h1>
+      </span>
     </div>
   );
 };
