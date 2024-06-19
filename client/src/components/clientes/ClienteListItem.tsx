84ce3c05c44846750be5af66f2de40955af5b7e5
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-19 17:56:42 -0600

    Action buttons

--- client/src/components/clientes/ClienteListItem.tsx
+++ client/src/components/clientes/ClienteListItem.tsx
@@ -1,6 +1,7 @@
 import { ClienteInterface } from "../../types/interfaces";
 import ListItem from "../utils/ListItem";
 import ActionButton from "../utils/ActionButton";
+import { Link } from "react-router-dom";
 
 const ClienteListItem = ({ cliente }: { cliente: ClienteInterface }) => {
   return (
@@ -35,9 +36,9 @@ const ClienteListItem = ({ cliente }: { cliente: ClienteInterface }) => {
         </div>
         <div className="w-full h-auto grid grid-cols-12">
           <div className="col-span-3 px-2 flex items-center">
-            <a href={`/clientes/${cliente.id}`} className="underline">
+            <Link to={`/clientes/${cliente.id}`} className="underline">
               {cliente.nombre}
-            </a>
+            </Link>
           </div>
           <div className="col-span-1 px-2 flex items-center">
             <p>{cliente.rfc}</p>
@@ -54,9 +55,53 @@ const ClienteListItem = ({ cliente }: { cliente: ClienteInterface }) => {
             <p>{cliente.empresa}</p>
           </div>
           <div className="col-start-11 col-span-full px-2 flex items-center">
-            <ActionButton title="Ver Cliente">Ver</ActionButton>
-            <ActionButton title="Editar Cliente">Editar</ActionButton>
-            <ActionButton title="Eliminar Cliente">Eliminar</ActionButton>
+            <ActionButton title="Ver Cliente">
+              <Link to={`/clientes/${cliente.id}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-eye-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+                  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
+                </svg>
+              </Link>
+            </ActionButton>
+            <ActionButton title="Editar Cliente">
+              <Link to={`/clientes/${cliente.id}/editar`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-pencil-square"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                  />
+                </svg>
+              </Link>
+            </ActionButton>
+            <ActionButton title="Eliminar Cliente">
+              <Link to={`/clientes/${cliente.id}/eliminar`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-trash-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                </svg>
+              </Link>
+            </ActionButton>
           </div>
         </div>
       </div>
