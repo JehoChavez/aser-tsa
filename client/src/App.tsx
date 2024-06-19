77bc97928531c5623a397aeb73996912cbe73983
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-18 18:17:33 -0600

    Initial clientes page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,17 @@ import Root from "./pages/Root";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Calendario from "./pages/Calendario";
+import Clientes from "./pages/Clientes";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [{ path: "/", element: <Calendario /> }],
+    children: [
+      { path: "/", element: <Calendario /> },
+      { path: "/clientes", element: <Clientes /> },
+    ],
   },
   { path: "/login", element: <Login /> },
   { path: "*", element: <NotFound /> },
---
+++ client/src/pages/Clientes.tsx
@@ -0,0 +1,9 @@
+const Clientes = () => {
+  return (
+    <div className="mt-16 w-full h-full">
+      <h1>Clientes Page</h1>
+    </div>
+  );
+};
+
+export default Clientes;
