a8a02b52101efff2044968dc059b71c0eb7ae3e5
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-19 16:56:27 -0600

    Better width for each item

--- client/src/components/clientes/ClientesListHeader.tsx
+++ client/src/components/clientes/ClientesListHeader.tsx
@@ -7,10 +7,10 @@ const ClientesListHeader = () => {
       <div className="w-full h-auto grid grid-cols-12 ">
         <p className="col-span-3 px-2">Nombre</p>
         <p className="col-span-1 px-2">RFC</p>
-        <p className="col-span-2 px-2">Correo</p>
+        <p className="col-span-3 px-2">Correo</p>
         <p className="col-span-1 px-2">Teléfono</p>
-        <p className="col-span-1 px-2">Empresa</p>
-        <p className="col-start-10 col-span-full px-2">Acción</p>
+        <p className="col-span-2 px-2">Empresa</p>
+        <p className="col-start-11 col-span-full px-2">Acción</p>
       </div>
     </div>
   );
