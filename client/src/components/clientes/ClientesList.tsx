be1ad38bfd6b27c172c6f5e0ba2d6203ea365eb5
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-24 19:34:41 -0600

    Lighter bg color

--- client/src/components/clientes/ClientesList.tsx
+++ client/src/components/clientes/ClientesList.tsx
@@ -5,7 +5,7 @@ import ClienteListItem from "./ClienteListItem";
 const ClientesList = () => {
   const clientesContext = useContext(ClientesContext);
   return (
-    <div className="h-5/6 w-full bg-neutral-200 overflow-y-auto">
+    <div className="h-5/6 w-full bg-neutral-100 overflow-y-auto">
       {clientesContext.clientes.map((cliente) => (
         <ClienteListItem cliente={cliente} key={cliente.id} />
       ))}
