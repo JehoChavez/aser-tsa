11ada7518a6df21ca9b22bcd358378ff40b29894
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-25 17:19:58 -0600

    Initial polizas page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Calendario from "./pages/Calendario";
 import Clientes from "./pages/Clientes";
 import Cliente from "./pages/Cliente";
+import Polizas from "./pages/Polizas";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -15,6 +16,7 @@ const router = createBrowserRouter([
       { path: "/", element: <Calendario /> },
       { path: "/clientes", element: <Clientes /> },
       { path: "/clientes/:id", element: <Cliente /> },
+      { path: "/polizas", element: <Polizas /> },
     ],
   },
   { path: "/login", element: <Login /> },
