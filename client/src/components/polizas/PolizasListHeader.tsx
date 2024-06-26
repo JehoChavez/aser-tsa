321a4516afd6007f966dc1ced66642751f3d1ab4
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-26 17:47:09 -0600

    Agente spans 2 cols

--- client/src/components/polizas/PolizasListHeader.tsx
+++ client/src/components/polizas/PolizasListHeader.tsx
@@ -3,7 +3,7 @@ const PolizasListHeader  = () => {
     <div className="w-full h-auto p-2 bg-blue-950 bg-opacity-90 text-neutral-200 grid grid-cols-4 md:grid-cols-12">
       <p className="col-span-1 md:col-span-2 lg:col-span-1 px-2">No. Póliza</p>
       <p className="hidden md:block md:col-span-2 xl:col-span-1 px-2">Aseguradora</p>
-      <p className="hidden lg:block col-span-2 2xl:col-span-1 px-2">Agente</p>
+      <p className="hidden lg:block col-span-2 px-2">Agente</p>
       <p className="hidden xl:block px-2">Vendedor</p>
       <p className="col-span-2 md:col-span-3 px-2">Contratante</p>
       <p className="hidden md:block px-2">Inicio Vigencia</p>
