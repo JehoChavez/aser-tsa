d5346bca7b6e3db0a70f6e0c65e043b30f282604
Jeho Chavez <chavezjehonatan@gmail.com>
2024-06-27 17:07:30 -0600

    Default prop values

--- client/src/components/utils/IconTextButton.tsx
+++ client/src/components/utils/IconTextButton.tsx
@@ -3,19 +3,13 @@ import { IconTextButtonInterface } from "../../types/interfaces";
 const IconTextButton = ({
   icon,
   children,
-  width,
-  height,
-  bgColor,
-  textColor,
-  hover,
+  width = "w-auto",
+  height = "heightAuto",
+  bgColor = "bg-blue-950",
+  textColor = "text-neutral-100",
+  hover = "bg-indigo-950",
   onClick,
 }: IconTextButtonInterface) => {
-  width = width || "w-auto";
-  height = height || "h-auto";
-  bgColor = bgColor || "bg-blue-950";
-  textColor = textColor || "text-neutral-100";
-  hover = hover || "bg-indigo-950";
-
   return (
     <button
       className={`${height} ${width} px-2 rounded flex items-center justify-evenly ${bgColor} ${textColor} hover:${hover}`}
