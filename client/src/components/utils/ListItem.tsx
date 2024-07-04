eaca4766ba2bfa73ce8e3c55771aba82dde7fa1f
Jeho Chavez <chavezjehonatan@gmail.com>
2024-07-04 16:59:36 -0600

    Bg color to ListItem

--- client/src/components/utils/ListItem.tsx
+++ client/src/components/utils/ListItem.tsx
@@ -2,7 +2,7 @@ import { ListItemProps } from "../../types/interfaces";
 
 const ListItem = ({ children }: ListItemProps) => {
   return (
-    <div className="w-full my-1 p-1 border border-slate-400 rounded">{children}</div>
+    <div className="w-full my-1 p-1 border border-slate-400 rounded bg-neutral-100">{children}</div>
   );
 };
 
