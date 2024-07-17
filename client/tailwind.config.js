c9719ec666d1bb397104883570b768f7e01f075f
Jeho Chavez <chavezjehonatan@gmail.com>
2024-07-17 17:29:23 -0600

    Custom styles to make number input appear as text input (no arrows)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,5 +4,19 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    function ({ addBase, config }) {
+      addBase({
+        'input[type="number"]::-webkit-outer-spin-button, input[type="number"]::-webkit-inner-spin-button':
+          {
+            "-webkit-appearance": "none",
+            margin: "0",
+          },
+        'input[type="number"]': {
+          "-moz-appearance": "textfield",
+        },
+      });
+    },
+  ],
 };
