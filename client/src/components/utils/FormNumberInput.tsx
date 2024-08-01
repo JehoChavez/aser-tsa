f689fe973abe40940dc09d946e142d58e8230cc9
Jeho Chavez <chavezjehonatan@gmail.com>
2024-07-31 19:17:12 -0600

    Only 2 digits after point

--- client/src/components/utils/FormNumberInput.tsx
+++ client/src/components/utils/FormNumberInput.tsx
@@ -14,7 +14,7 @@ const FormNumberInput = ({
       <label htmlFor={name}>{label}</label>
       <input
         type="number"
-        step="any"
+        step="0.01"
         name={name}
         id={name}
         className="h-8 p-1 bg-neutral-100 border-gray-400 rounded focus:ring-blue-400 focus:ring-2 disabled:bg-opacity-80 disabled:border-opacity-80"
