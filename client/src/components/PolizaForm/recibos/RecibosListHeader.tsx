c29eba92ec65cee0a8964d785f77684bfe69f84e
Jeho Chavez <chavezjehonatan@gmail.com>
2024-09-02 18:03:13 -0600

    Complete RecibosListHeader component

--- client/src/components/PolizaForm/recibos/RecibosListHeader.tsx
+++ client/src/components/PolizaForm/recibos/RecibosListHeader.tsx
@@ -6,12 +6,30 @@ const RecibosListHeader = () => {
 
   return (
     <div className="h-auto p-2 bg-blue-950 bg-opacity-90 text-neutral-200 flex rounded">
-      <div className="w-full grid grid-cols-9">
-        <span className="col-span-1 flex justify-around font-bold text-2xl">
-          <button onClick={formRecibosContext.subNrOfRecibos}>-</button>
+      <div className="w-full grid grid-cols-9 text-xl text-center">
+        <span className="col-span-1 flex justify-evenly">
+          <button
+            className="font-bold"
+            onClick={formRecibosContext.subNrOfRecibos}
+          >
+            -
+          </button>
           <p>{formRecibosContext.nrOfRecibos}</p>
-          <button onClick={formRecibosContext.addNrOfRecibos}>+</button>
+          <button
+            className="font-bold"
+            onClick={formRecibosContext.addNrOfRecibos}
+          >
+            +
+          </button>
         </span>
+        <p>Prima Neta</p>
+        <p>Expedición</p>
+        <p>Financiamieto</p>
+        <p>IVA</p>
+        <p>Otros</p>
+        <p>Prima Total</p>
+        <p>Fecha Inicio</p>
+        <p>Fecha Límite</p>
       </div>
     </div>
   );
