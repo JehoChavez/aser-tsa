d79c67c3d80197a84e480b04caaa35b2ea4fd01e
Jeho Chavez <chavezjehonatan@gmail.com>
2024-09-09 17:27:54 -0600

    PostPolizaPayload type

--- client/src/types/types.tsx
+++ client/src/types/types.tsx
@@ -5,3 +5,10 @@ export type ValueType = Date | null;
 export type DatePickerValue = ValueType | [ValueType, ValueType];
 
 export type DayPendientes = Recibo[] | Renovacion[] | null;
+
+export type PostPolizaPayload = {
+  poliza: {
+    [k: string]: FormDataEntryValue;
+  };
+  recibos: Recibo[];
+};
