74640eddae9492b3fb8a751614ce28ae70abf372
Jeho Chavez <chavezjehonatan@gmail.com>
2024-09-18 16:38:01 -0600

    ErrorModal component

---
+++ client/src/components/utils/ErrorModal.tsx
@@ -0,0 +1,30 @@
+import ActionButton from "./ActionButton";
+import Modal from "./Modal";
+
+const ErrorModal = ({ onClick }: { onClick?: () => void }) => {
+  return (
+    <Modal size="small">
+      <div className="w-full flex justify-center mt-3 text-red-700">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="150"
+          height="150"
+          fill="currentColor"
+          className="bi bi-x-circle-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+        </svg>
+      </div>
+      <h4 className="text-center text-3xl mt-3">Algo salió mal</h4>
+      <p className="text-center text-lg">Intenta de nuevo más tarde</p>
+      <div className="w-full flex justify-center mt-2">
+        <ActionButton onClick={onClick} color="blue" size="lg">
+          OK
+        </ActionButton>
+      </div>
+    </Modal>
+  );
+};
+
+export default ErrorModal;
