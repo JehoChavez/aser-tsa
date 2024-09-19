744294829274c39e4bc7e87bf48cebcbd9e12ff1
Jeho Chavez <chavezjehonatan@gmail.com>
2024-09-18 18:20:43 -0600

    Semibold data titles for better styling

--- client/src/components/clientes/ClienteInfo.tsx
+++ client/src/components/clientes/ClienteInfo.tsx
@@ -14,31 +14,68 @@ const ClienteInfo = ({ cliente }: { cliente: ClienteInterface }) => {
       </h3>
       <div className="flex flex-col md:flex-row w-full p-2 text-neutral-700">
         <div className="md:w-1/3 m-1">
-          <p>Tipo: Persona {cliente?.tipoPersona}</p>
-          <p>
-            {cliente.tipoPersona === "fisica"
-              ? "Fecha de nacimiento:"
-              : "Fecha de constitución:"}{" "}
-            {nacimiento}
-          </p>
-          <p>RFC: {cliente.rfc}</p>
-          <p>Empresa: {cliente.empresa}</p>
-          <p>
-            Correo:{" "}
-            <a href={`mailto:${cliente.correo}`} className="underline">
-              {cliente.correo}
-            </a>
-          </p>
-          <p>Teléfono: {cliente.telefono}</p>
+          <div>
+            <p className="inline-block font-semibold mr-1">Tipo:</p>
+            <p className="inline-block">Persona {cliente?.tipoPersona}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">
+              {cliente.tipoPersona === "fisica"
+                ? "Fecha de nacimiento:"
+                : "Fecha de constitución:"}
+            </p>
+            <p className="inline-block">{nacimiento}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">RFC:</p>
+            <p className="inline-block">{cliente.rfc}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">Empresa:</p>
+            <p className="inline-block">{cliente.empresa}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">Correo:</p>
+            <p className="inline-block">
+              <a href={`mailto:${cliente.correo}`} className="underline">
+                {cliente.correo}
+              </a>
+            </p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">Teléfono:</p>
+            <p className="inline-block">{cliente.telefono}</p>
+          </div>
         </div>
         <div className="md:w-1/3 m-1">
-          <p>Calle: {cliente.calle}</p>
-          <p>Exterior: {cliente.exterior}</p>
-          <p>Interior: {cliente.interior}</p>
-          <p>Colonia: {cliente.colonia}</p>
-          <p>CP: {cliente.codigoPostal}</p>
-          <p>Estado: {cliente.estado?.estado}</p>
-          <p>Municipio: {cliente.municipio?.municipio}</p>
+          <div>
+            <p className="inline-block font-semibold mr-1">Calle:</p>
+            <p className="inline-block">{cliente.calle}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">Exterior:</p>
+            <p className="inline-block">{cliente.exterior}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">Interior:</p>
+            <p className="inline-block">{cliente.interior}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">Colonia:</p>
+            <p className="inline-block">{cliente.colonia}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">Código Postal:</p>
+            <p className="inline-block">{cliente.codigoPostal}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">Estado:</p>
+            <p className="inline-block">{cliente.estado?.estado}</p>
+          </div>
+          <div>
+            <p className="inline-block font-semibold mr-1">Municipio:</p>
+            <p className="inline-block">{cliente.municipio?.municipio}</p>
+          </div>
         </div>
         <div className="md:w-1/3 m-1 flex flex-col">
           <h4 className="font-semibold">Comentarios:</h4>
