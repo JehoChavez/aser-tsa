0ea0842d0e964f7a6d031e2660c3a0517085516d
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-02 17:06:55 -0600

    Disabled option in DropdownButton

--- client/src/components/utils/DropdownButton.tsx
+++ client/src/components/utils/DropdownButton.tsx
@@ -3,14 +3,17 @@ import { ReactNode } from "react";
 const DropdownButton = ({
   onClick,
   children,
+  disabled,
 }: {
   onClick: () => void;
   children: ReactNode;
+  disabled?: boolean;
 }) => {
   return (
     <button
-      className="w-full hover:bg-gray-200 active:bg-gray-500 active:text-white rounded text-left px-1"
-      onClick={onClick}
+      className="w-full hover:bg-gray-200 active:bg-gray-500 active:text-white rounded text-left px-1 disabled:bg-gray-200 disabled:text-gray-500"
+      onClick={disabled ? () => {} : onClick}
+      disabled={disabled}
     >
       {children}
     </button>
