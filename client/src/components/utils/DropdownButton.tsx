3ea1c76684d0e9f9aee53f46caa0ffe396c163ed
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-03 17:59:53 -0600

    Not allowed cursor when dropdown button is disabled

--- client/src/components/utils/DropdownButton.tsx
+++ client/src/components/utils/DropdownButton.tsx
@@ -11,7 +11,7 @@ const DropdownButton = ({
 }) => {
   return (
     <button
-      className="w-full hover:bg-gray-200 active:bg-gray-500 active:text-white rounded text-left px-1 disabled:bg-gray-200 disabled:text-gray-500"
+      className="w-full hover:bg-gray-200 active:bg-gray-500 active:text-white rounded text-left px-1 disabled:bg-gray-200 disabled:text-gray-500 disabled:hover:cursor-not-allowed"
       onClick={disabled ? () => {} : onClick}
       disabled={disabled}
     >
