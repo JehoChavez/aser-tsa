e5dc1e8361e4b31650904e50cec6c3a041fe3a15
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-09 16:39:23 -0600

    More complete LabelAndData component

--- client/src/components/utils/LabelAndData.tsx
+++ client/src/components/utils/LabelAndData.tsx
@@ -8,11 +8,9 @@ const LabelAndData = ({
   children: ReactNode;
 }) => {
   return (
-    <div>
-      <span>
-        <p>{label}</p>
-      </span>
-      {children}
+    <div className="bg-neutral-100 p-1 rounded text-neutral-500 w-full">
+      <p className="border-b border-neutral-300 mb-1">{label}</p>
+      <span className="px-1 text-neutral-700">{children}</span>
     </div>
   );
 };
