1d883b68e0eb5bd8d38057c99f33f8ffe9d78eac
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-09 16:39:45 -0600

    Add margin to the top

--- client/src/components/utils/PolizaSection.tsx
+++ client/src/components/utils/PolizaSection.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const PolizaSection = ({ children }: { children: ReactNode }) => {
-  return <div className="flex flex-col md:flex-row mb-4 w-full">{children}</div>;
+  return <div className="flex flex-col md:flex-row my-4 w-full">{children}</div>;
 };
 
 export default PolizaSection;
