726a15c34b6a0b130c4fc3c2a3a0a43663348006
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-09 17:17:37 -0600

    Remove unnecesary div

--- client/src/components/poliza/PolizaNumberVigenciaSection.tsx
+++ client/src/components/poliza/PolizaNumberVigenciaSection.tsx
@@ -14,25 +14,23 @@ const PolizaNumberVigenciaSection = ({
 }) => {
   return (
     <PolizaSection>
-      <div className="w-full flex flex-col md:flex-row">
-        <div className="md:w-1/4 m-1">
-          <LabelAndData label="Póliza">{noPoliza}</LabelAndData>
-        </div>
-        <div className="md:w-1/4 mx-1">
-          <LabelAndData label="Fecha de Emisión">
-            {emision.split("-").reverse().join("-")}
-          </LabelAndData>
-        </div>
-        <div className="md:w-1/4 mx-1">
-          <LabelAndData label="Inicio de Vigencia">
-            {inicioVigencia.split("-").reverse().join("-")}
-          </LabelAndData>
-        </div>
-        <div className="md:w-1/4 mx-1">
-          <LabelAndData label="Fin de Vigencia">
-            {finVigencia.split("-").reverse().join("-")}
-          </LabelAndData>
-        </div>
+      <div className="md:w-1/4 m-1">
+        <LabelAndData label="Póliza">{noPoliza}</LabelAndData>
+      </div>
+      <div className="md:w-1/4 m-1">
+        <LabelAndData label="Fecha de Emisión">
+          {emision.split("-").reverse().join("-")}
+        </LabelAndData>
+      </div>
+      <div className="md:w-1/4 m-1">
+        <LabelAndData label="Inicio de Vigencia">
+          {inicioVigencia.split("-").reverse().join("-")}
+        </LabelAndData>
+      </div>
+      <div className="md:w-1/4 m-1">
+        <LabelAndData label="Fin de Vigencia">
+          {finVigencia.split("-").reverse().join("-")}
+        </LabelAndData>
       </div>
     </PolizaSection>
   );
