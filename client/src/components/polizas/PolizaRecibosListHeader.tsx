1107db5c6eda0493bf2f2735e01fb0420d55f379
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-15 17:20:53 -0600

    Add responsiveness to PolizaReciboListHeader

--- client/src/components/polizas/PolizaRecibosListHeader.tsx
+++ client/src/components/polizas/PolizaRecibosListHeader.tsx
@@ -16,16 +16,16 @@ const PolizaRecibosListHeader = ({
 
   return (
     <div className="w-full bg-blue-950 text-gray-200">
-      <div className="flex justify-between p-2">
-        <div>
+      <div className="md:flex justify-between p-2">
+        <div className="my-2 md:my-0">
           <p className="text-sm text-neutral-300">Póliza</p>
           <span>{noPoliza}</span>
         </div>
-        <div>
+        <div className="my-2 md:my-0">
           <p className="text-sm text-neutral-300">Contratante</p>
           <span>{contratante}</span>
         </div>
-        <div className="flex">
+        <div className="flex justify-between my-2 md:my-0">
           <div className="mx-3">
             <p className="text-sm text-neutral-300">Prima Neta</p>
             <span>${currencyFormatter.format(primaNeta)}</span>
