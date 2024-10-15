6a5ad07ce61125a0a3a8d75c475ae06d4f46f14e
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-14 18:39:51 -0600

    Recibos list PENDING different component por poliza recibos

--- client/src/components/polizas/PolizaRecibosDialog.tsx
+++ client/src/components/polizas/PolizaRecibosDialog.tsx
@@ -2,6 +2,9 @@ import Modal from "../utils/Modal";
 import { PolizaRecibosContext } from "../../store/poliza-recibos-context";
 import { useContext, useEffect } from "react";
 import Loading from "../utils/Loading";
+import ListDialog from "../utils/ListDialog";
+import PendientesReciboListHeader from "../pendientes/PendientesReciboListHeader";
+import PendientesReciboListItem from "../pendientes/PendientesReciboListItem";
 
 const PolizaRecibosDialog = () => {
   const polizaRecibosContext = useContext(PolizaRecibosContext);
@@ -9,11 +12,18 @@ const PolizaRecibosDialog = () => {
   useEffect(() => {
     polizaRecibosContext.fetchRecibos();
   }, []);
-  console.log(polizaRecibosContext.recibos);
 
   return (
     <Modal size="large" onClose={polizaRecibosContext.onClose} closeBtn>
-      {polizaRecibosContext.isLoading && <Loading />}
+      {polizaRecibosContext.isLoading ? (
+        <Loading />
+      ) : (
+        <ListDialog title="Recibos" header={<PendientesReciboListHeader />}>
+          {polizaRecibosContext.recibos.map((recibo) => (
+            <PendientesReciboListItem recibo={recibo} key={recibo.id} />
+          ))}
+        </ListDialog>
+      )}
     </Modal>
   );
 };
