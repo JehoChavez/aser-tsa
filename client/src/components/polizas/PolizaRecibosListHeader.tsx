54d5f5fddc94b0c8dc08116dcfff5f88be264be2
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-16 16:51:09 -0600

    Reduce col number

--- client/src/components/polizas/PolizaRecibosListHeader.tsx
+++ client/src/components/polizas/PolizaRecibosListHeader.tsx
@@ -36,12 +36,12 @@ const PolizaRecibosListHeader = ({
           </div>
         </div>
       </div>
-      <div className="hidden md:grid grid-cols-12 w-full text-wrap p-2 text-center">
+      <div className="hidden md:grid grid-cols-9 w-full text-wrap p-2 text-center">
         <h3>Endoso</h3>
         <h3>Serie</h3>
-        <h3 className="col-span-3">Inicio de Vigencia</h3>
-        <h3 className="col-span-3">Límite de Pago</h3>
-        <h3 className="col-span-3">Fecha de Pago</h3>
+        <h3 className="col-span-2">Inicio de Vigencia</h3>
+        <h3 className="col-span-2">Límite de Pago</h3>
+        <h3 className="col-span-2">Fecha de Pago</h3>
         <h3>Acción</h3>
       </div>
     </div>
