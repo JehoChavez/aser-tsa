3026576bbb191ae5c9fe1070d774c3397d0c9796
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-21 16:59:18 -0600

    Bg and text colors

--- client/src/components/polizas/PolizaRecibosListCols.tsx
+++ client/src/components/polizas/PolizaRecibosListCols.tsx
@@ -1,6 +1,6 @@
 const PolizaRecibosListCols = () => {
   return (
-    <div className="hidden md:grid grid-cols-10 w-full text-wrap p-2 text-center">
+    <div className="hidden md:grid grid-cols-10 w-full text-wrap p-2 text-center bg-blue-950 text-gray-200">
       <h3>Endoso</h3>
       <h3>Serie</h3>
       <h3 className="col-span-2">Inicio de Vigencia</h3>
