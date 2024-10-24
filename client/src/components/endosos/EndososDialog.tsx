8e418f78763e04fbb8e933b8db11360729282373
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-24 16:22:12 -0600

    Header and crear endoso buttons

--- client/src/components/endosos/EndososDialog.tsx
+++ client/src/components/endosos/EndososDialog.tsx
@@ -1,4 +1,5 @@
 import Modal from "../utils/Modal";
+import ActionButton from "../utils/ActionButton";
 
 const EndososDialog = ({
   id,
@@ -9,7 +10,17 @@ const EndososDialog = ({
 }) => {
   return (
     <Modal size="large" closeBtn onClose={onClose}>
-      Endosos
+      <h2 className="w-full text-center text-2xl bg-blue-950 text-gray-100 font-bold p-1">
+        ENDOSOS
+      </h2>
+      <div className="p-2">
+        <h3 className="mb-2 text-neutral-900">Crear</h3>
+        <div className="flex flex-col md:flex-row md:w-96 md:Sjustify-between">
+          <ActionButton color="blue">Endoso A</ActionButton>
+          <ActionButton color="blue">Endoso B</ActionButton>
+          <ActionButton color="blue">Endoso D</ActionButton>
+        </div>
+      </div>
     </Modal>
   );
 };
