d0575fed503689bf8d79a5c106984ee98d6b55f5
Jeho Chavez <chavezjehonatan@gmail.com>
2024-10-24 18:02:55 -0600

    Provide endosos context, fetch endosos, loading state and error handling

--- client/src/components/endosos/EndososDialog.tsx
+++ client/src/components/endosos/EndososDialog.tsx
@@ -1,6 +1,10 @@
 import Modal from "../utils/Modal";
 import ActionButton from "../utils/ActionButton";
 import EndososList from "./EndososList";
+import { EndososContext } from "../../store/endosos-context";
+import { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
+import Loading from "../utils/Loading";
 
 const EndososDialog = ({
   id,
@@ -11,6 +15,39 @@ const EndososDialog = ({
   noPoliza: string;
   onClose: () => void;
 }) => {
+  const [endosos, setEndosos] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  const fetchEndosos = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/polizas/${id}/endosos`,
+        {
+          withCredentials: true,
+        }
+      );
+      setEndosos(response.data.content);
+    } catch (error) {
+      setHasError(true);
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 401) {
+          setIsAuthenticated(false);
+        }
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchEndosos();
+  }, []);
+
   return (
     <Modal size="large" closeBtn onClose={onClose}>
       <h2 className="w-full text-center text-2xl bg-blue-950 text-gray-100 font-bold p-1">
@@ -24,7 +61,33 @@ const EndososDialog = ({
           <ActionButton color="blue">Endoso D</ActionButton>
         </div>
       </div>
-      <EndososList />
+      {isLoading ? (
+        <Loading />
+      ) : hasError ? (
+        <div className="w-full h-full flex justify-center">
+          <div className="text-center flex align-middle justify-center flex-col">
+            <div className="flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                fill="currentColor"
+                className="bi bi-x-circle text-red-700"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+              </svg>
+            </div>
+            <h3 className="text-2xl font-bold">Algo Salió Mal</h3>
+            <h4>Intenta de Nuevo Más Tarde</h4>
+          </div>
+        </div>
+      ) : (
+        <EndososContext.Provider value={{ endosos, fetchEndosos }}>
+          <EndososList />
+        </EndososContext.Provider>
+      )}
     </Modal>
   );
 };
