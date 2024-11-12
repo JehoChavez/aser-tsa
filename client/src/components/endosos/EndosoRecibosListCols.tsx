e5fd72a2fdfc62abec15d8e7efddcf670653e566
Jeho Chavez <chavezjehonatan@gmail.com>
2024-11-11 18:33:01 -0600

    Gray text color

--- client/src/components/endosos/EndosoRecibosListCols.tsx
+++ client/src/components/endosos/EndosoRecibosListCols.tsx
@@ -1,6 +1,6 @@
 const EndosoRecibosListCols = () => {
   return (
-    <div className="hidden md:grid grid-cols-9 w-full text-wrap p-2 text-center bg-blue-950">
+    <div className="hidden md:grid grid-cols-9 w-full text-wrap p-2 text-center bg-blue-950 text-gray-200">
       <h3>Serie</h3>
       <h3 className="col-span-2">Inicio de Vigencia</h3>
       <h3 className="col-span-2">Límite de Pago</h3>
