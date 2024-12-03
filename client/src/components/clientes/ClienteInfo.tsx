55a39278b113a6f08858af99ac1591efa92c7152
Jeho Chavez <chavezjehonatan@gmail.com>
2024-12-03 16:41:08 -0600

    Edit cliente button in ClienteInfo

--- client/src/components/clientes/ClienteInfo.tsx
+++ client/src/components/clientes/ClienteInfo.tsx
@@ -1,4 +1,6 @@
 import { ClienteInterface } from "../../types/interfaces";
+import ActionButton from "../utils/ActionButton";
+import { Link } from "react-router-dom";
 
 const ClienteInfo = ({ cliente }: { cliente: ClienteInterface }) => {
   let nacimiento: string | undefined;
@@ -9,9 +11,29 @@ const ClienteInfo = ({ cliente }: { cliente: ClienteInterface }) => {
 
   return (
     <div className="bg-neutral-100 p-2 rounded">
-      <h3 className="text-xl font-semibold text-neutral-800">
-        Datos del cliente
-      </h3>
+      <div className="flex justify-between">
+        <h3 className="text-xl font-semibold text-neutral-800">
+          Datos del cliente
+        </h3>
+        <Link to={`/clientes/${cliente.id}/editar`}>
+          <ActionButton title="Editar Cliente">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-pencil-square"
+              viewBox="0 0 16 16"
+            >
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+              <path
+                fillRule="evenodd"
+                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+              />
+            </svg>
+          </ActionButton>
+        </Link>
+      </div>
       <div className="flex flex-col md:flex-row w-full p-2 text-neutral-700">
         <div className="md:w-1/3 m-1">
           <div>
