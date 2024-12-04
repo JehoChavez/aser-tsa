09407547c9e552de8de4597816ac8875047a5f71
Jeho Chavez <chavezjehonatan@gmail.com>
2024-12-04 17:04:56 -0600

    Nuevo Cliente button in SelectClienteModal

--- client/src/components/clientes/SelectClienteModal.tsx
+++ client/src/components/clientes/SelectClienteModal.tsx
@@ -1,11 +1,12 @@
 import { useState, useCallback, useEffect } from "react";
 import Modal from "../utils/Modal";
 import axios, { AxiosError } from "axios";
-import { Navigate } from "react-router-dom";
+import { Navigate, Link } from "react-router-dom";
 import { ClienteInterface, ClientesSearchParams } from "../../types/interfaces";
 import SearchInput from "../utils/SearchInput";
 import Loading from "../utils/Loading";
 import ListItem from "../utils/ListItem";
+import IconTextButton from "../utils/IconTextButton";
 
 const SelectClienteModal = ({
   onClose,
@@ -85,6 +86,28 @@ const SelectClienteModal = ({
             );
           })
         )}
+      </div>
+      <div className="flex justify-end pt-1">
+        <Link to="/clientes/nuevo">
+          <IconTextButton
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-person-fill-add mr-2"
+                viewBox="0 0 16 16"
+              >
+                <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4" />
+              </svg>
+            }
+            height="h-8"
+          >
+            Nuevo
+          </IconTextButton>
+        </Link>
       </div>
     </Modal>
   );
