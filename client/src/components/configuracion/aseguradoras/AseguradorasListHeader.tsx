b0f2d8fedb24fedef5df4005ffa5727198935b67
Jeho Chavez <chavezjehonatan@gmail.com>
2024-12-11 17:49:15 -0600

    Include acciones in header

--- client/src/components/configuracion/aseguradoras/AseguradorasListHeader.tsx
+++ client/src/components/configuracion/aseguradoras/AseguradorasListHeader.tsx
@@ -1,10 +1,11 @@
 const AseguradorasListHeader = () => {
   return (
-    <div className="w-full h-auto text-center p-2 bg-blue-950 text-neutral-200 hidden md:grid md:grid-cols-5 rounded-b">
+    <div className="w-full h-auto text-center p-2 bg-blue-950 text-neutral-200 hidden md:grid md:grid-cols-6 rounded-b">
       <p className="hidden md:block">Aseguradora</p>
       <p className="hidden md:block">Plazo Primer Recibo</p>
       <p className="hidden md:block">Plazo Recibos Subsecuentes</p>
       <p className="hidden md:block col-span-2">Comentarios</p>
+      <p className="hidden md:block">Acciones</p>
     </div>
   );
 };
