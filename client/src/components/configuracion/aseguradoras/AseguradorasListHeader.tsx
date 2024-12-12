a52fe0c1d301da4b1b82a64ea78d3ab468262beb
Jeho Chavez <chavezjehonatan@gmail.com>
2024-12-11 18:33:10 -0600

    Styling modifications to AseguradorasListHeader

--- client/src/components/configuracion/aseguradoras/AseguradorasListHeader.tsx
+++ client/src/components/configuracion/aseguradoras/AseguradorasListHeader.tsx
@@ -1,10 +1,10 @@
 const AseguradorasListHeader = () => {
   return (
-    <div className="w-full h-auto text-center p-2 bg-blue-950 text-neutral-200 hidden md:grid md:grid-cols-6 rounded-b">
+    <div className="w-full h-auto p-2 bg-blue-950 text-neutral-200 hidden md:grid md:grid-cols-6 rounded-b">
       <p className="hidden md:block">Aseguradora</p>
-      <p className="hidden md:block">Plazo Primer Recibo</p>
-      <p className="hidden md:block">Plazo Recibos Subsecuentes</p>
-      <p className="hidden md:block col-span-2">Comentarios</p>
+      <p className="hidden md:block text-center">Plazo Primer Recibo</p>
+      <p className="hidden md:block text-center">Plazo Recibos Subsecuentes</p>
+      <p className="hidden md:block col-span-2 text-center">Comentarios</p>
       <p className="hidden md:block">Acciones</p>
     </div>
   );
