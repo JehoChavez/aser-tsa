225cd8cdbb5a8c8971b97adb8412dace91691cf6
Jeho Chavez <chavezjehonatan@gmail.com>
2025-01-15 18:16:52 -0600

    Include count in CustomResponse to send count of rows

--- server/utils/CustomResponse.js
+++ server/utils/CustomResponse.js
@@ -1,8 +1,9 @@
 class CustomResponse {
-  constructor(data, status = 200, message = "success") {
+  constructor(data, status = 200, message = "success", count = undefined) {
     this.message = message;
     this.status = status;
     this.content = data;
+    this.count = count;
 
     if (status === 201 && message === "success") {
       this.message = "created";
