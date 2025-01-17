aac57d96bfa180f5ad5073f00bc23550c47ca8fd
Jeho Chavez <chavezjehonatan@gmail.com>
2025-01-16 18:38:42 -0600

    Set count state after fetching and display PageControls

--- client/src/pages/Polizas.tsx
+++ client/src/pages/Polizas.tsx
@@ -13,6 +13,7 @@ import Loading from "../components/utils/Loading";
 import PolizasListHeader from "../components/polizas/PolizasListHeader";
 import PolizasList from "../components/polizas/PolizasList";
 import ErrorModal from "../components/utils/ErrorModal";
+import PageControls from "../components/utils/PageControls";
 
 const Polizas = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -24,6 +25,8 @@ const Polizas = () => {
     defaultPolizasContext.params
   );
 
+  const [count, setCount] = useState(0);
+
   const fetchPolizas = useCallback(async () => {
     setIsLoading(true);
     try {
@@ -32,6 +35,7 @@ const Polizas = () => {
         withCredentials: true,
       });
       setPolizas(response.data.content);
+      setCount(response.data.count);
       setIsLoading(false);
     } catch (error) {
       console.log(error);
@@ -70,52 +74,57 @@ const Polizas = () => {
   if (!isAuthenticated) return <Navigate to="/login" replace />;
 
   return (
-    <PolizasContext.Provider
-      value={{
-        polizas,
-        params,
-        fetchPolizas,
-        onSearch,
-        setParams,
-      }}
-    >
-      {isError && <ErrorModal onClick={() => setIsError(false)} />}
-      <div className="w-full h-full px-5 py-4 flex flex-col overflow-hidden">
-        <IconTitle
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              fill="currentColor"
-              className="bi bi-file-earmark-medical-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1m-3 2v.634l.549-.317a.5.5 0 1 1 .5.866L7 7l.549.317a.5.5 0 1 1-.5.866L6.5 7.866V8.5a.5.5 0 0 1-1 0v-.634l-.549.317a.5.5 0 1 1-.5-.866L5 7l-.549-.317a.5.5 0 0 1 .5-.866l.549.317V5.5a.5.5 0 1 1 1 0m-2 4.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1m0 2h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1" />
-            </svg>
-          }
-        >
-          Pólizas
-        </IconTitle>
-        <PolizasOptions />
-        <div className="w-full h-full flex flex-col overflow-hidden">
-          <PolizasListHeader />
-          {isLoading ? (
-            <Modal size="small">
-              <Loading />
-            </Modal>
-          ) : polizas[0] ? (
-            <>
-              <PolizasList />
-            </>
-          ) : (
-            <>
-              <h3 className="text-center">No hay pólizas</h3>
-            </>
-          )}
+    <>
+      <PolizasContext.Provider
+        value={{
+          polizas,
+          params,
+          fetchPolizas,
+          onSearch,
+          setParams,
+        }}
+      >
+        {isError && <ErrorModal onClick={() => setIsError(false)} />}
+        <div className="w-full h-full px-5 py-4 flex flex-col overflow-hidden">
+          <IconTitle
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className="bi bi-file-earmark-medical-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1m-3 2v.634l.549-.317a.5.5 0 1 1 .5.866L7 7l.549.317a.5.5 0 1 1-.5.866L6.5 7.866V8.5a.5.5 0 0 1-1 0v-.634l-.549.317a.5.5 0 1 1-.5-.866L5 7l-.549-.317a.5.5 0 0 1 .5-.866l.549.317V5.5a.5.5 0 1 1 1 0m-2 4.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1m0 2h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1" />
+              </svg>
+            }
+          >
+            Pólizas
+          </IconTitle>
+          <PolizasOptions />
+          <div className="w-full h-full flex flex-col overflow-hidden">
+            <PolizasListHeader />
+            {isLoading ? (
+              <Modal size="small">
+                <Loading />
+              </Modal>
+            ) : polizas[0] ? (
+              <>
+                <PolizasList />
+              </>
+            ) : (
+              <>
+                <h3 className="text-center">No hay pólizas</h3>
+              </>
+            )}
+          </div>
         </div>
+      </PolizasContext.Provider>
+      <div className="w-full flex justify-center mb-8">
+        <PageControls />
       </div>
-    </PolizasContext.Provider>
+    </>
   );
 };
 
