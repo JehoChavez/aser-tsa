56ec8bfd735e771a9bddca9196c52cbaf1d8e846
Jeho Chavez <chavezjehonatan@gmail.com>
2025-03-31 16:40:36 -0600

    Include onClose function

--- client/src/components/configuracion/MassiveUploadDialog.tsx
+++ client/src/components/configuracion/MassiveUploadDialog.tsx
@@ -2,11 +2,13 @@ import Modal from "../utils/Modal";
 
 const MassiveUploadDialog = ({
   type,
+  onClose,
 }: {
   type: "aseguradoras" | "agentes" | "vendedores" | "ramos";
+  onClose: () => void;
 }) => {
   return (
-    <Modal size="small" closeBtn>
+    <Modal size="small" closeBtn onClose={onClose}>
       <h3 className="text-2xl bg-blue-950 text-neutral-50 rounded p-2">
         Carga masiva de {type}
       </h3>
