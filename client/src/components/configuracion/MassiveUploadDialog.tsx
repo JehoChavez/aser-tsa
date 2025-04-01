3fb566d01db6c724d7e6bc1a13bd7a5fb1bc609d
Jeho Chavez <chavezjehonatan@gmail.com>
2025-04-01 12:24:43 -0600

    Use Response and Loading components

--- client/src/components/configuracion/MassiveUploadDialog.tsx
+++ client/src/components/configuracion/MassiveUploadDialog.tsx
@@ -3,6 +3,8 @@ import Modal from "../utils/Modal";
 import ActionButton from "../utils/ActionButton";
 import axios, { AxiosError } from "axios";
 import { Navigate } from "react-router-dom";
+import Response from "./Response";
+import Loading from "../utils/Loading";
 
 const MassiveUploadDialog = ({
   type,
@@ -13,6 +15,8 @@ const MassiveUploadDialog = ({
 }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [response, setResponse] = useState<any>(null);
+  const [results, setResults] = useState<any[]>([]);
+  const [errors, setErrors] = useState<any[]>([]);
   const [isAuthenticated, setIsAuthenticated] = useState(true);
 
   const handleFormSubmission = async (
@@ -39,11 +43,16 @@ const MassiveUploadDialog = ({
           },
         }
       );
+      setResults(request.data.content.results);
+      setErrors(request.data.content.errors);
       setResponse(request.data.content);
     } catch (error) {
       if (error instanceof AxiosError) {
         if (error.response?.status === 401) {
           setIsAuthenticated(false);
+        } else {
+          setErrors(error.response?.data.content.errors);
+          setResponse(error.response?.data.content);
         }
       }
     } finally {
@@ -54,30 +63,38 @@ const MassiveUploadDialog = ({
   if (!isAuthenticated) return <Navigate to="/login" replace />;
 
   return (
-    <Modal size="small" closeBtn onClose={onClose}>
+    <Modal size={response ? "medium" : "small"} closeBtn onClose={onClose}>
       <h3 className="text-2xl bg-blue-950 text-neutral-50 rounded p-2">
         Carga masiva de {type}
       </h3>
-      <form
-        className="h-full flex flex-col justify-between items-center p-2"
-        onSubmit={handleFormSubmission}
-      >
-        <div className="flex flex-col h-full justify-center items-center">
-          <input
-            type="file"
-            accept=".csv"
-            name="file"
-            id="file"
-            className="p-1"
-            required
-          />
+      {isLoading ? (
+        <div className="h-full flex justify-center items-center">
+          <Loading />
         </div>
-        <div className="w-full flex justify-end">
-          <ActionButton size="lg" color="blue">
-            Subir
-          </ActionButton>
-        </div>
-      </form>
+      ) : response ? (
+        <Response results={results} errors={errors} />
+      ) : (
+        <form
+          className="h-full flex flex-col justify-between items-center p-2"
+          onSubmit={handleFormSubmission}
+        >
+          <div className="flex flex-col h-full justify-center items-center">
+            <input
+              type="file"
+              accept=".csv"
+              name="file"
+              id="file"
+              className="p-1"
+              required
+            />
+          </div>
+          <div className="w-full flex justify-end">
+            <ActionButton size="lg" color="blue">
+              Subir
+            </ActionButton>
+          </div>
+        </form>
+      )}
     </Modal>
   );
 };
