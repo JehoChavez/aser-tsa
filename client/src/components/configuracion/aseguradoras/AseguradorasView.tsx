3e7bb9ca2a3ea807e501c7ae37a49bb5ed93202a
Jeho Chavez <chavezjehonatan@gmail.com>
2025-04-01 16:47:28 -0600

    Refactor button rendering logic in AgentesView, AseguradorasView, RamosView, and VendedoresView components

--- client/src/components/configuracion/aseguradoras/AseguradorasView.tsx
+++ client/src/components/configuracion/aseguradoras/AseguradorasView.tsx
@@ -75,27 +75,33 @@ const AseguradorasView = () => {
       )}
       <ButtonPortal>
         <div className="flex flex-col align-center mt-2">
-          <IconTextButton
-            icon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-plus-circle-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
-              </svg>
-            }
-            width="w-full"
-            height="h-full"
-            onClick={handleClick}
-            onRightClick={handleRightClick}
-          >
-            Nueva
-          </IconTextButton>
-          {showUploadButton && <UploadButton onClick={handleUploadClick} />}
+          {showUploadButton ? (
+            <UploadButton
+              onClick={handleUploadClick}
+              onRightClick={handleRightClick}
+            />
+          ) : (
+            <IconTextButton
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-plus-circle-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                </svg>
+              }
+              width="w-full"
+              height="h-full"
+              onClick={handleClick}
+              onRightClick={handleRightClick}
+            >
+              Nuevo
+            </IconTextButton>
+          )}
         </div>
       </ButtonPortal>
       <AseguradorasContext.Provider value={{ aseguradoras, fetchAseguradoras }}>
