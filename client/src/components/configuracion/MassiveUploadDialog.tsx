5586de5fe08d6ede1439beb6c67d11bfde1ea8ef
Jeho Chavez <chavezjehonatan@gmail.com>
2025-03-31 18:59:59 -0600

    File submission. Pending response processing.

--- client/src/components/configuracion/MassiveUploadDialog.tsx
+++ client/src/components/configuracion/MassiveUploadDialog.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import Modal from "../utils/Modal";
 import ActionButton from "../utils/ActionButton";
+import axios, { AxiosError } from "axios";
+import { Navigate } from "react-router-dom";
 
 const MassiveUploadDialog = ({
   type,
@@ -8,29 +11,73 @@ const MassiveUploadDialog = ({
   type: "aseguradoras" | "agentes" | "vendedores" | "ramos";
   onClose: () => void;
 }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [response, setResponse] = useState<any>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  const handleFormSubmission = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const file = formData.get("file") as File;
+    if (!file) return;
+
+    try {
+      setIsLoading(true);
+
+      const uploadData = new FormData();
+      uploadData.append("file", file);
+
+      const request = await axios.post(
+        `http://localhost:3000/api/${type}/upload`,
+        uploadData,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "multipart/form-data", // Ensure the correct content type
+          },
+        }
+      );
+      setResponse(request.data.content);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 401) {
+          setIsAuthenticated(false);
+        }
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
+
   return (
     <Modal size="small" closeBtn onClose={onClose}>
       <h3 className="text-2xl bg-blue-950 text-neutral-50 rounded p-2">
         Carga masiva de {type}
       </h3>
-      <div className="flex flex-col h-full justify-center items-center">
-        <input
-          type="file"
-          accept=".csv"
-          name="archivo"
-          id="archivo"
-          className="p-1"
-        />
-      </div>
-      <ActionButton
-        color="blue"
-        onClick={() => {
-          // Handle file upload logic here
-          console.log("File uploaded");
-        }}
+      <form
+        className="h-full flex flex-col justify-between items-center p-2"
+        onSubmit={handleFormSubmission}
       >
-        Subir
-      </ActionButton>
+        <div className="flex flex-col h-full justify-center items-center">
+          <input
+            type="file"
+            accept=".csv"
+            name="file"
+            id="file"
+            className="p-1"
+            required
+          />
+        </div>
+        <div className="w-full flex justify-end">
+          <ActionButton size="lg" color="blue">
+            Subir
+          </ActionButton>
+        </div>
+      </form>
     </Modal>
   );
 };
