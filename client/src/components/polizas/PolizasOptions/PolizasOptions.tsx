d8938dcf3cd3d5ec0b660f5f9672721e765dac5a
Jeho Chavez <chavezjehonatan@gmail.com>
2025-04-02 18:03:19 -0600

    Massive polizas upload

--- client/src/components/polizas/PolizasOptions/PolizasOptions.tsx
+++ client/src/components/polizas/PolizasOptions/PolizasOptions.tsx
@@ -10,6 +10,8 @@ import VendedorDropdown from "./VendedorDropdown";
 import RamoDropdown from "./RamoDropdown";
 import FechaDropdown from "./FechaDropdown";
 import OrderDropdown from "./OrderDropdown";
+import UploadButton from "../../configuracion/UploadButton";
+import MassiveUploadDialog from "../../configuracion/MassiveUploadDialog";
 
 const PolizasOptions = () => {
   const { id: clienteId } = useParams();
@@ -17,6 +19,17 @@ const PolizasOptions = () => {
   const polizasContext = useContext(PolizasContext);
   const [showClienteSelect, setShowClienteSelect] = useState(false);
   const [isClientePage, setIsClientePage] = useState(false);
+
+  const [showUploadButton, setShowUploadButton] = useState(false);
+  const [showMassiveUpload, setShowMassiveUpload] = useState(false);
+
+  const handleRightClick = () => {
+    setShowUploadButton((prevState) => !prevState);
+  };
+
+  const handleUploadClick = () => {
+    setShowMassiveUpload(true);
+  };
 
   const nuevaClickHandler = () => {
     if (clienteId) {
@@ -37,27 +50,41 @@ const PolizasOptions = () => {
   return (
     <div className="w-full min-h-40 md:min-h-0 h-1/5 flex items-center">
       {showClienteSelect && <SelectClienteModal onClose={closeModalHandler} />}
+      {showMassiveUpload && (
+        <MassiveUploadDialog
+          type="polizas"
+          onClose={() => setShowMassiveUpload(false)}
+        />
+      )}
       <div className="w-full h-1/2 grid grid-cols-4 md:grid-cols-5 xl:grid-cols-12">
         <div className="flex items-center col-span-1 px-2">
-          <IconTextButton
-            icon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="currentColor"
-                className="bi bi-file-earmark-plus-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0" />
-              </svg>
-            }
-            width="w-full"
-            height="h-8"
-            onClick={nuevaClickHandler}
-          >
-            Nueva
-          </IconTextButton>
+          {showUploadButton ? (
+            <UploadButton
+              onClick={handleUploadClick}
+              onRightClick={handleRightClick}
+            />
+          ) : (
+            <IconTextButton
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-file-earmark-plus-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0" />
+                </svg>
+              }
+              width="w-full"
+              height="h-8"
+              onClick={nuevaClickHandler}
+              onRightClick={handleRightClick}
+            >
+              Nueva
+            </IconTextButton>
+          )}
         </div>
         <div className="col-span-3 md:col-span-2 flex items-center px-2">
           <SearchInput
