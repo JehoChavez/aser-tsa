35d5649cf6cc9d2772efb851d896a6301588eaa9
Jeho Chavez <chavezjehonatan@gmail.com>
2025-04-08 10:40:16 -0600

    Toggle password visibility

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -8,6 +8,11 @@ const LoginForm = () => {
   const [isError, setIsError] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   const formSubmitHandler = async (event: FormEvent) => {
     event.preventDefault();
 
@@ -51,17 +56,32 @@ const LoginForm = () => {
         <label htmlFor="password" className="w-full text-left mb-1">
           Contraseña
         </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          className={`w-full rounded-md p-1 ring-1 ring-inset ${
-            isIncorrect ? "ring-red-500" : "ring-gray-300"
-          }`}
-          placeholder="password"
-          required
-          ref={passwordRef}
-        />
+        <div className="flex items-center w-full">
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            id="password"
+            className={`w-full rounded-md p-1 ring-1 ring-inset ${
+              isIncorrect ? "ring-red-500" : "ring-gray-300"
+            }`}
+            placeholder="password"
+            required
+            ref={passwordRef}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-eye ml-2 cursor-pointer"
+            viewBox="0 0 16 16"
+            onClick={togglePasswordVisibility}
+          >
+            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+          </svg>
+        </div>
+
         <p
           className={`w-full text-sm text-red-500 ${
             isIncorrect ? "block" : "hidden"
