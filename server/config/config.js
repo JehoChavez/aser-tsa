58c7e945a32d5c72e3b116f66000180ee8cdbc2a
Jeho Chavez <chavezjehonatan@gmail.com>
2025-04-09 17:54:29 -0600

    Sequelize config file

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASS,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
+  migration: {
+    username: process.env.DB_ADMIN_USER,
+    password: process.env.DB_ADMIN_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+};
